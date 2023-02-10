928672a52427e7def14e33a8d290893d4011a42e
Garrett Ross <garrettross@gGarrettRoss.attlocal.net>
2023-02-10 16:32:47 -0600

    Working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+
 
 function App() {
+
+  // hooks
+  const [loggedIn, setLoggedIn] = useState(false)
+
+  if(loggedIn) {
+    return <h1>Welcome back</h1>
+  }
+
+  const handleLogIn = () => {
+    setLoggedIn(!loggedIn)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Access denied</h1>
+      <button onClick={handleLogIn}>Log in</button>
     </div>
-  );
+  )
 }
 
 export default App;
